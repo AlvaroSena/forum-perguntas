7187de65e426fcd69038b0eaef44d7761a225d8a
Alvaro Sena <alvarosenacs.c@gmail.com>
2026-02-17 14:31:36 -0300

    feat: add create account controller and add zod validation pipe

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,45 @@
+import { ConflictException, UsePipes } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { hash } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createAccountSchema = z.object({
+  name: z.string(),
+  email: z.email(),
+  password: z.string(),
+});
+
+type CreateAccountSchema = z.infer<typeof createAccountSchema>;
+
+@Controller("/accounts")
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountSchema))
+  async handle(@Body() body: CreateAccountSchema) {
+    const { name, email, password } = body;
+
+    const userAlreadyExists = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new ConflictException("E-mail already in use.");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+      },
+    });
+  }
+}
